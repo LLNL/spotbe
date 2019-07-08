362814f649b8758fd5e1b82a72a1620ec98bf2b2
Joseph Chavez <chavez35@llnl.gov>
2019-07-08 11:51:29 -0700

    get rid of pathlb stuff

--- spot.py
+++ spot.py
@@ -1,5 +1,4 @@
 import argparse, json, sys, pickle, os, subprocess, getpass, urllib
-from pathlib import Path
 from functools import partial
 import math
 
@@ -14,7 +13,6 @@ def _sub_call(cmd):
 
 def _cali_func_duration(inclus_dur, filepath):
     return _sub_call([CALIQUERY , '-q', 'SELECT function,{0} WHERE function FORMAT JSON'.format(inclus_dur), str(filepath) ])
-    #return  _sub_call([CALIQUERY , '-q', 'SELECT function,{0} FORMAT JSON'.format(inclus_dur), str(filepath) ])
 
 
 def _cali_func_topdown(filepath):
@@ -25,20 +23,19 @@ def _cali_list_globals(inclus_dur, filepath):
     cali_globals = _sub_call( [CALIQUERY ,'-j', '--list-globals', str(filepath) ])[0]
 
     # duration needs to be added to cali globals... just get from select call for now
-    # just finds the highest duration in all the functions, assuming that would be the root
-    if inclus_dur:
-        cali_globals["Inclusive Duration"] = max( map( lambda item: item.get(inclus_dur, 0) 
-                                                       , _cali_func_duration(inclus_dur, filepath)))
+    # just finds the highest duration in all the functions, assuming that would be the root 
+    cali_globals["Inclusive Duration"] = max( map( lambda item: item.get(inclus_dur, 0) 
+                                                 , _cali_func_duration(inclus_dur, filepath)))
     return cali_globals 
 
 
 def hierarchical(args):
-    dirpath    = Path(args.directory)
+    dirpath    = args.directory
     inclus_dur = args.durationKey
 
     #load cache or initiate if missing
     cache = []
-    fnames = list(map(lambda fname: dirpath / fname, args.filenames) if args.filenames else [fpath for fpath in dirpath.iterdir() if fpath.name.endswith('.cali')] )
+    fnames = list(map(lambda fname: os.path.join(dirpath , fname), args.filenames) if args.filenames else [os.path.join(dirpath, fpath) for fpath in os.listdir(dirpath) if fpath.endswith('.cali')] )
 
     import multiprocessing
     metaList = multiprocessing.Pool(18).map(partial(_cali_list_globals, inclus_dur), fnames)
@@ -51,107 +48,8 @@ def hierarchical(args):
     json.dump(out, sys.stdout)
 
 
-# returns a single durations hierarchy given a filepath
-def durations(args):
-    filepath = Path(args.filepath)
-    inclus_dur = args.durationKey
-    data_list = _cali_func_duration(inclus_dur, filepath)
-    output = {}
-    for item in data_list: 
-        func_path = item["function"]
-        duration = item.get(inclus_dur, 0)
-        output[func_path] = max(duration, output.get(func_path, 0))
-    json.dump(output, sys.stdout)   
-
-#def hierarchical2(args):
-#    dirpath    = Path(args.directory)
-#    cache_path = dirpath / "spot_cache2.pkl"
-#
-#    #load cache or initiate if missing
-#    cache = []
-#    if cache_path.exists():
-#        try: cache = pickle.load(cache_path.open('rb'))
-#        except: pass
-#    else:
-#        cache_path.touch()
-#        os.chown(cache_path, -1 , os.stat(dirpath).st_gid)
-#        cache_path.chmod(0o660)
-#
-#    cached_filenames = list(map(lambda item: item['meta']['Filename'], cache))
-#
-#    # check for new cali files, if so add to cache and write to disk
-#    cache_miss_fnames = [fpath for fpath in dirpath.iterdir() 
-#                          if fpath.name.endswith('.cali') and not fpath.name in cached_filenames] 
-#    if cache_miss_fnames:
-#        import multiprocessing
-#        metaList = multiprocessing.Pool().map(_cali_list_globals, cache_miss_fnames)
-#        dataList = multiprocessing.Pool().map(_cali_func_duration, cache_miss_fnames)
-#        dataList = map(lambda item: {entry['function']: entry['sum#time.inclusive.duration'] for entry in item}, dataList)
-#
-#        cache = [*cache, *[{'meta': m, 'data': d} for (m, d) in zip(metaList, dataList)]]
-#        pickle.dump(cache, cache_path.open('wb'))
-#
-#    # dump summary stdout
-#    json.dump(cache, sys.stdout)
-
-
-
-
-#def durations2(args):
-#    import multiprocessing
-#
-#    filepaths = list(filter(lambda fp: fp.endswith('.cali'), args.filepaths))
-#    if args.directory:
-#        filepaths = list(map(lambda fp: os.path.join(args.directory, fp), filepaths))
-#    file_datas = multiprocessing.Pool().map(_cali_func_duration, filepaths)
-#
-#    # create an dictionary of empty lists keyed by function path
-#    durations = { item["function"]:[] for item in file_datas[0] }
-#
-#    for file_data in file_datas:
-#        duration_by_func_path = {}
-#        for item in file_data: 
-#            func_path = item["function"]
-#            duration = item[INCLUS_DURATION]
-#            duration_by_func_path[func_path] = max(duration, duration_by_func_path.get(func_path, 0))
-#        for (func_path, duration) in duration_by_func_path.items():
-#            durations[func_path].append(duration)
-#
-#    json.dump({"filepaths": filepaths, "durationLists": [{"funcPath": f, "durationList": d} for (f,d) in durations.items()]}, sys.stdout)
-
-def removeChart():
-    pass
-
-
-def summary(args):
-    dirpath    = Path(args.filepath)
-    cache_path = dirpath / "spot_cache.pkl"
-
-    #load cache or initiate if missing
-    cache = {}
-    if cache_path.exists():
-        try: cache = pickle.load(cache_path.open('rb'))
-        except: pass
-    else:
-        cache_path.touch()
-        os.chown(cache_path, -1 , os.stat(dirpath).st_gid)
-        cache_path.chmod(0o660)
-    inclus_dur = None
-
-    # check for new cali files, if so add to cache and write to disk
-    cache_miss_fpaths = [str(fpath) for fpath in dirpath.iterdir() if not fpath.name in cache and fpath.name.endswith('.cali')]
-    if cache_miss_fpaths:
-        import multiprocessing
-        cache = {**cache, **dict(zip(cache_miss_fpaths, multiprocessing.Pool().map( partial(_cali_list_globals, inclus_dur), cache_miss_fpaths)))}
-        pickle.dump(cache, cache_path.open('wb'))
-    
-    # layout: if filename provided then return contents,  else generate a generic one
-    layout = ""
-    if args.layout:
-        layout = json.load(open(args.layout))
-
-    else:
-        def getChartItem(meta):
+def _generateLayout(metasFromFilenameDict):
+        def genChartItem(meta):
             name = meta[0]
             val = meta[1]
             try:
@@ -162,24 +60,116 @@ def summary(args):
 
             return {"dimension": name, "title": name, "viz": viz}
 
-        def getTableItem(meta):
+        def genTableItem(meta):
             name = meta[0]
             return {"dimension": name, "label": name}
 
-        metas     = next(iter((cache.values()))).items()
-        chartList = list(map(getChartItem, metas))
-        tableList = list(map(getTableItem, metas))
-        layout = {"charts": chartList, "table": tableList}
+        metas     = next(iter((metasFromFilenameDict.values()))).items()
+        chartList = list(map(genChartItem, metas))
+        tableList = list(map(genTableItem, metas))
+        return {"charts": chartList, "table": tableList}
+
+
+# returns a single durations hierarchy given a filepath
+def durations(args):
+    filepath = args.filepath
+    inclus_dur = args.durationKey
+    data_list = _cali_func_duration(inclus_dur, filepath)
+    output = {}
+    for item in data_list: 
+        func_path = item["function"]
+        duration = item.get(inclus_dur, 0)
+        output[func_path] = max(duration, output.get(func_path, 0))
+    json.dump(output, sys.stdout)   
+
+def toggleChart(args):
+
+    dirpath = args.dirpath
+    chartname = args.chartname
+    show = True if args.show == 'true' else False
+    
+    # load spot settings from user home dir
+    spot_settings_filepath = os.path.expanduser('~/spot_settings.pk')
+
+    spot_settings = {}
+    try: spot_settings = pickle.load(open(spot_settings_filepath, 'rb'))
+    except: pass
+
+    # toggle setting
+    hide_list = []
+    if spot_settings.get(dirpath, None):
+        if spot_settings[dirpath].get('hide', None):
+            # already have the list in settings, repoint to it:
+            hide_list = spot_settings[dirpath]['hide'] 
+        else:
+            # don't have it, so add it
+            spot_settings[dirpath]['hide'] = hide_list
+    else:
+        spot_settings[dirpath] = {'hide': hide_list}
+
+    if (show): 
+        try:
+            hide_list.remove(chartname)
+        except:
+            pass
+    else: 
+        if chartname not in hide_list:
+            hide_list.append(chartname)
+
+    # save settings to user home dir
+    pickle.dump(spot_settings, open(spot_settings_filepath, 'wb'))
+
+    
+def summary(args):
+    dirpath    = args.filepath
+    cache_path = os.path.join(dirpath , "spot_cache.pkl")
+
+    #load cache or initiate if missing
+    cache = {}
+    if os.path.exists(cache_path):
+        try: cache = pickle.load(open(cache_path,'rb'))
+        except:  pass
+    else:
+        open(cache_path, 'a').close()  # touch file
+        os.chown(cache_path, -1 , os.stat(dirpath).st_gid)
+        os.chmod(cache_path, 0o660)
+
+
+    # check for new cali files, if so add to cache and write to disk
+    cache_miss_fpaths = [fname for fname in os.listdir(dirpath) if not fname in cache and fname.endswith('.cali')]
+    if cache_miss_fpaths:
+        import multiprocessing
+        cache = {**cache, **dict(zip(cache_miss_fpaths, multiprocessing.Pool(18).map( partial(_cali_list_globals, None), [os.path.join(dirpath, fname) for fname in cache_miss_fpaths])))}
+        pickle.dump(cache, open(cache_path, 'wb'))
+
+    
+    # layout: if filename provided then return contents,  else generate a generic one
+    layout = ""
+    if args.layout:
+        layout = json.load(open(args.layout))
+
+    else:
+        layout = _generateLayout(cache)
+
+    # filter out hidden results:
+    spot_settings_filepath = os.path.expanduser('~/spot_settings.pk')
+
+    hide = None
+    try: 
+        hide = pickle.load(open(spot_settings_filepath, 'rb')).get(cache_path, None)
+    except: pass
+    
 	
+    filter(lambda el: el['dimension'] not in hide, layout['charts'])
+    filter(lambda el: el['dimension'] not in hide, layout['table'])
 
     # dump summary stdout
     json.dump({'data': cache, 'layout': layout}, sys.stdout)
 
 
-
 def topdown(args):
     """call cali on topdown file and return a json object with function keys and objects with duration and topdown info"""
-    filepath = Path(args.filepath)
+    filepath = args.filepath
     json.dump( {item["function"]:{ "duration": item["count"] 
                                  , "topdown": {k[15:]:v for (k,v) in item.items() if k.startswith("libpfm.topdown#")} 
                                  } 
@@ -222,15 +212,16 @@ summary_sub.add_argument("--layout", help="layout json filepath")
 #summary_sub.add_argument("--layout", help="layout json filepath", default="/usr/gapps/wf/web/spot/data/default_layout.json")
 summary_sub.set_defaults(func=summary)
 
+removeChart_sub = subparsers.add_parser("toggleChart")
+removeChart_sub.add_argument("dirpath", help="directory path of data to toggle chart")
+removeChart_sub.add_argument("chartname", help="chartname to toggle")
+removeChart_sub.add_argument("show", help="either true or false")
+removeChart_sub.set_defaults(func=toggleChart)
+
 durations_sub = subparsers.add_parser("durations")
 durations_sub.add_argument("durationKey", help="the key for the inclusive duration")
 durations_sub.add_argument("filepath", help="file and directory paths")
 durations_sub.set_defaults(func=durations)
-
-#durations2_sub = subparsers.add_parser("durations2")
-#durations2_sub.add_argument("filepaths", nargs="+", help="one or more filepaths")
-#durations2_sub.add_argument("--directory", help="prepend to each filepath")
-#durations2_sub.set_defaults(func=durations2)
 
 hierarchical_sub = subparsers.add_parser("hierarchical")
 hierarchical_sub.add_argument("directory", help="directory")
