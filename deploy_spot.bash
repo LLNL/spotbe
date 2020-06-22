db962046c2be7d9f3326919445f561e27e63683f
Pascal Aschwanden <aschwanden1@llnl.gov>
2020-06-22 10:36:13 -0700

    Add deploy spot script to repo.  we will use this to deploy to live now.  still needs live testing

---
+++ deploy_spot.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DIR=/usr/gapps/spot/
+DEV=$DIR"dev/"
+LIVE=$DIR"live/"
+HATCHET=$LIVE"hatchet/"
+CALIPER=$LIVE"caliper/"
+
+echo 'DEV      = '$DEV
+echo 'LIVE     = '$LIVE
+echo 'HATCHET  = '$HATCHET 
+echo 'CALIPER  = '$CALIPER
+echo '-----------------------------------'
+
+cd $LIVE
+
+ls $HATCHET
+ls $CALIPER
+
+exit
+
+asdf
+sleep 10000
+echo 'Now deploying...'
+
+git fetch; 
+git reset --hard origin/develop; 
+chmod -R 755 web; 
+cat web/js/Environment.js
+
+# now copy hatchet to live
+cp -r $DEV"hatchet/*" $HATCHET
+
+# now copy caliper to live
+cp -r $DEV"caliper/*" $CALIPER
+
+
+
+
