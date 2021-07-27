f679f32c018b8e044b13778d98e59277772ccf00
Pascal Aschwanden <aschwanden1@llnl.gov>
2021-07-27 16:12:27 -0700

    implement multiple directory checks, using self.check_dir.  Also, implement exception handling

--- CustomTemplates.py
+++ CustomTemplates.py
@@ -8,12 +8,22 @@ class CustomTemplates:
         self.multi_notebooks = []
 
     def get(self, sf):
+        try:
+            self.get_files(sf)
+        except Exception as e:
+            print(e)
+
+    def get_files(self, sf):
 
         self.check_home_dir()
         self.check_templates_dir()
         self.check_sf_dir( sf )
 
-        jnote = json.dumps( self.notebooks )
+        combined = {}
+        combined["single"] = self.notebooks
+        combined["multi"] = self.multi_notebooks
+
+        jnote = json.dumps( combined )
 
         pprint( jnote )
         return jnote
@@ -30,22 +40,27 @@ class CustomTemplates:
 
 
     def check_sf_dir(self, sf):
-        self.check_dir( sf, self.notebooks )
+        self.check_dir( sf + "/single", self.notebooks )
+        self.check_dir( sf + "/multi", self.multi_notebooks )
 
 
     def check_templates_dir(self):
 
-        temps_dir = "/usr/gapps/spot/templates"
+        temps_dir = "/usr/gapps/spot/templates/single"
         self.check_dir(temps_dir, self.notebooks)
+
+        multi_dir = "/usr/gapps/spot/templates/multi"
+        self.check_dir(temps_dir, self.multi_notebooks)
 
 
     def check_home_dir(self):
 
         from os.path import expanduser
         home = expanduser("~")        
-        homedir = home + "/notebooks"
+        homedir = home + "/notebooks/single"
 
         self.check_dir(homedir, self.notebooks)
 
-        multi_homedir = home + "/multi_notebooks"
+        multi_homedir = home + "/notebooks/multi"
+        self.check_dir(multi_homedir, self.multi_notebooks)
 
