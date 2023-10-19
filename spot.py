481d35f326290c4fab8c2c01343949c22ceced28
Pascal Aschwanden <aschwanden1@llnl.gov>
2023-10-18 17:13:19 -0700

    fix broken sym link to python that no longer exists by using the new venv I created

--- spot.py
+++ spot.py
@@ -1,4 +1,6 @@
-#!/usr/gapps/spot/venv_python/bin/python3
+#!/usr/gapps/spot/dev/venv_2023/bin/python3
+
+###/usr/gapps/spot/venv_python/bin/python3
 import argparse, json, sys, os, platform, subprocess, getpass, urllib.parse, socket, time
 import cProfile
 import hashlib
